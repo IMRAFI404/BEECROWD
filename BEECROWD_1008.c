90510f59d6a021a8c4883062776f064a60f71025
Iftamam Mortaza Rafi <155842367+IMRAFI404@users.noreply.github.com>
2024-01-25 00:02:28 +0600

    BEECROWD_1008.c

---
+++ BEECROWD_1008.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int A ,B;
+    double C,D;
+    scanf("%d%",&A);
+    printf("NUMBER = %d\n",A);
+    scanf("%d%lf",&B,&C);
+    D=B*C;
+    printf("SALARY = U$ %.2lf\n",D);
+
+
+    return 0;
+}
